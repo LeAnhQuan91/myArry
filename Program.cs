e83c904ef5661c37b2e4bdc7b83c16038b600143
LeAnhQuan91 <quanthao03019791@gmail.com>
2024-04-09 19:28:02 +0700

    myArry
    myArry

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+
+class Program
+    {
+public static void Main(string[] args)
+{
+int [] myArry = {1, 2, 3, 4,5 ,6, 7, 8, 9,10} ;
+int sum = 0 ;
+for(int i = 0;i < myArry.Length; i++)
+{
+    sum+= myArry[i] ;
+    //Console.Write($"Tong cua mang la:{sum+=i}");// in lien tuc
+}
+Console.Write($"Tong cua mang la:{sum}"); // in mot lan
+            }
+    }
+
